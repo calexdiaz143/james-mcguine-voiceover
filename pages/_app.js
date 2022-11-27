395d7056957c2cad5ca4f6191b0f048e2464ca52
Cesar Diaz <calexdiaz143@gmail.com>
2022-11-27 00:50:32 -0800

    Set up index with herolayout1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 
+import { Amplify } from 'aws-amplify'
+import config from '../src/aws-exports'
+
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+import { studioTheme } from '../src/ui-components'
+
+import '@aws-amplify/ui-react/styles.css'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AmplifyProvider theme={studioTheme}>
+      <Component {...pageProps} />
+    </AmplifyProvider>
+  )
 }
 
 export default MyApp
